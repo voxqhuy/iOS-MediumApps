340a37ffc6a5279f392b520a22ec116c8224d921
voxqhuy <voxqhuy@gmail.com>
2018-05-11 14:45:31 -0400

    added Homeowner app that has a table view

---
+++ Home-Owner/Item.swift
@@ -0,0 +1,22 @@
+//
+//  Item.swift
+//  Home-Owner
+//
+//  Created by Vo Huy on 5/11/18.
+//  Copyright © 2018 Vo Huy. All rights reserved.
+//
+
+import UIKit
+
+class Item: NSObject {
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    let dateCreated: Date
+    
+    init(name: String, valueInDollars: Int, serialNumber: String?) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+    }
+}
---
+++ Home-Owner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Home-Owner
+//
+//  Created by Vo Huy on 5/11/18.
+//  Copyright © 2018 Vo Huy. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
