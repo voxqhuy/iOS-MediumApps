a0bd3e959709cb810f5b4391ebca16b4bc3e7be4
voxqhuy <voxqhuy@gmail.com>
2018-05-28 20:33:27 -0400

    added photoInfoViewController for displaying a single image

--- Pictogram/Pictogram/View Controllers/PhotoInfoViewController.swift
+++ Pictogram/Pictogram/View Controllers/PhotoInfoViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class PhotoInfoViewController: UIViewController {
     
-    
+    // MARK: - Properties
+    var photo: Photo! {
+        didSet {
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    // MARK: Outlet
+    @IBOutlet var imageView: UIImageView!
 
+    
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        store.fetchImage(for: photo) {
+            (result) in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -22,16 +41,5 @@ class PhotoInfoViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
